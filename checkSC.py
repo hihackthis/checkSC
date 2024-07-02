2e3a7c627d64279e75bcd6be4b2733a956ad3018
hihackthis <hihackthis@gmail.com>
2024-07-02 13:39:19 -0300

    Add gitignore

--- checkSC.py
+++ checkSC.py
@@ -1,45 +1,42 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import webbrowser
-import re
 
-# providing url 
+# Fetch ShellCheck Wiki
 url = urlopen('https://www.shellcheck.net/wiki/')
 bs = BeautifulSoup(url.read(), 'html.parser')
 
+# Search function ShellCheck code number
 def BinarySearch(lst, x):
     ini = 0
-    fim = len(lst) - 1
+    end = len(lst) - 1
     flag = False
-    while (ini <= fim and not flag):
-        mid = (ini + fim) // 2
+    while (ini <= end and not flag):
+        mid = (ini + end) // 2
         if lst[mid] == x:
             flag = True
         elif x > lst[mid]:
             ini = mid +1
         else:
-            fim = mid-1
+            end = mid-1
     if flag == True:
         urlSC = 'https://www.shellcheck.net/wiki/'
         fullUrl = urlSC + x
-        #print(fullUrl)
         webbrowser.open_new(fullUrl)
     else:
         print("\nKey is not found")
 
-#codes = bs.find_all('a', href=True)
-#code = bs.select('a[href^="SC"]')
-#print(code)
-
+# List all the ShellCheck code number
 link_href = [ x.get("href") for x in bs.select('a[href^="SC"]') ]
-#print(link_href)
 
+# User input ShellCheck code number
 x = input('\nEnter with the code (SCXXXX): ')
 BinarySearch(link_href, x)
 
-while True:   
-    ch = input("\nNew research? Press Y, if no, press ENTER: ")    
-    if ch == 'Y':
+# Search infinite loop
+while True:
+    ch = input("\nNew research? Press Y/y, if no, press ENTER: ")
+    if ch == 'Y' or ch == 'y':
         x = input('\nEnter with the code (SCXXXX): ')
         BinarySearch(link_href, x)
     elif ch == "":
